fc3c561ede9b754bd5e6bdc0e6f815dbd03eed9a
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-23 19:27:37 -0300

    :sparkles: add create_users_table migration file

---
+++ priv/repo/migrations/20210523160505_create_users_table.exs
@@ -0,0 +1,21 @@
+defmodule Rockelivery.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table :users do
+      add :address,        :string
+      add :age,            :integer
+      add :cep,            :string
+      add :cpf,            :string
+      add :email,          :string
+      add :name,           :string
+      add :password_hash,  :string
+
+      timestamps()
+    end
+
+    # create index("users", [:email], unique: true)
+    create unique_index(:users, [:cpf])
+    create unique_index(:users, [:email])
+  end
+end
