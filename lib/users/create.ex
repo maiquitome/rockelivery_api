098c1bcf8f3f5a3d0972c2f5400314cd41dca1d6
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-06 22:25:45 -0300

    :sparkles: add handle_insert

--- lib/users/create.ex
+++ lib/users/create.ex
@@ -5,7 +5,14 @@ defmodule Rockelivery.Users.Create do
     params
     |> User.changeset()
     |> Repo.insert()
+    |> handle_insert()
   end
 
   def call(_anything), do: "Enter the data in a map format"
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, changeset}) do
+    {:error, %{status: :internal_server_error, result: changeset}}
+  end
 end
