c208aac73f43a0cb3ee17b07e2299bbbe396b601
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-11 20:34:10 -0300

    :sparkles: add delete file

---
+++ lib/rockelivery/users/delete.ex
@@ -0,0 +1,33 @@
+defmodule Rockelivery.Users.Delete do
+  @moduledoc """
+  Delete an user from the database.
+  """
+
+  alias Ecto.Changeset
+  alias Rockelivery.{Error, Repo, User}
+
+  @spec call(binary) ::
+          {:error, %{result: String.t(), status: :bad_request}}
+          | {:error, %{result: String.t(), status: :not_found}}
+          | {:ok, %User{}}
+          | {:error, %Changeset{}}
+  @doc """
+  Delete an user from the database.
+
+  ## Examples
+
+      iex> user_params = %{address: "Rua...", age: 28, cep: "12345678", cpf: "12345678910",
+      email: "teste_teste@teste.com", name: "Maiqui Tomé", password: "123456"}
+
+      iex> {:ok, %Rockelivery.User{} = user} = Rockelivery.create_user(user_params)
+
+      iex> {:ok, %Rockelivery.User{}} = Rockelivery.Users.Delete.call(user.id)
+
+  """
+  def call(id) do
+    case Repo.get(User, id) do
+      nil -> {:error, Error.build_user_not_found()}
+      user_schema -> Repo.delete(user_schema)
+    end
+  end
+end
