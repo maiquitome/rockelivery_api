354c2071258d64dce329a219a9ceae0dc6be74e4
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-11 20:22:11 -0300

    :sparkles: add error file

---
+++ lib/rockelivery/error.ex
@@ -0,0 +1,33 @@
+defmodule Rockelivery.Error do
+  alias Ecto.Changeset
+  alias Rockelivery.Error
+
+  @keys [:status, :result]
+
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  @spec build(atom(), String.t() | %Changeset{}) :: %Error{
+          result: String.t() | %Changeset{},
+          status: atom()
+        }
+  @doc """
+  Build error messages.
+  """
+  def build(status, result) do
+    %__MODULE__{
+      result: result,
+      status: status
+    }
+  end
+
+  @doc """
+  Error default message for status :not_found.
+  """
+  @spec build_user_not_found :: %Error{
+          result: String.t(),
+          status: :not_found
+        }
+  def build_user_not_found, do: build(:not_found, "User not found")
+end
