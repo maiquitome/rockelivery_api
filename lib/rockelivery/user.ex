060ab06a14846880aca4eed9c34a30b05762e488
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-13 13:32:08 -0300

    :recycle: change password_hash to password

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -56,7 +56,7 @@ defmodule Rockelivery.User do
     struct
     |> cast(params, @fields_that_can_be_changed)
     |> validate_required(fields)
-    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:password, min: 6)
     |> validate_length(:cep, is: 8)
     |> validate_length(:cpf, is: 11)
     |> validate_number(:age, greater_than_or_equal_to: 18)
